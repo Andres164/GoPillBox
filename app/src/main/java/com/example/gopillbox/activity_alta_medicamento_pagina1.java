0365222e8a44cf3878c16db772262b4ed1cf60d9
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-12-12 18:57:00 -0700

    Added the Android studio project with almost all the windows

---
+++ app/src/main/java/com/example/gopillbox/activity_alta_medicamento_pagina1.java
@@ -0,0 +1,14 @@
+package com.example.gopillbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_alta_medicamento_pagina1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alta_medicamento_pagina1);
+    }
+}
---
+++ app/src/main/java/com/example/gopillbox/activity_alta_medicamento_pagina2.java
@@ -0,0 +1,13 @@
+package com.example.gopillbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_alta_medicamento_pagina2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
